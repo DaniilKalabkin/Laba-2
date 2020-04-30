c077d355db30f355106dd5683a09d06237a7ce77
DaniilKalabkin <63413904+DaniilKalabkin@users.noreply.github.com>
2020-04-30 13:23:24 +0300

    Добавьте файлы проекта.

---
+++ Laba 2/Laba 2.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void f1(int* mass, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cin >> mass[i];
+    }
+
+}
+int main()
+{
+    int n;
+    cout << "Razmer Massiva" << endl;
+    cin >> n;
+    int* mass = new int[n];
+    f1(mass, n);
+    int maxcount(1), nn(-1);
+
+    for (int i = 0; i < n; i++)
+    {
+        int count(0);
+
+        for (int j = i; j < n; j++)
+
+            if (mass[i] == mass[j])
+                count++;
+        //if(maxcount == count)   //На случай если 2 максимально повтор элемента или более 
+            //cout<<mass[i];
+
+        if (maxcount < count)
+        {
+            maxcount = count;
+            nn = i;
+        }
+    }
+
+
+    if (nn > -1)
+
+        cout << endl << mass[nn] << endl;
+    else
+
+        cout << endl << "Netu!" << endl;
+    delete[] mass;
+    return 0;
+}
